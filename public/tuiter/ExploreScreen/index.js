470b83970e871b93f553bdf84e8b5d6d53688a79
Bobby Doshi <bob@Bobbys-MacBook-Air.local>
2022-10-14 10:39:50 -0400

    Move explore/ to ExploreScreen/

---
+++ public/tuiter/ExploreScreen/index.js
@@ -0,0 +1,18 @@
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
+import ExploreComponent from "./ExploreComponent.js";
+
+function exploreComponent() {
+    $('#wd-explore').append(`
+        <div class="col-2 col-lg-1 col-xl-2 py-2">
+            ${NavigationSidebar()}
+        </div>
+         <div class="col-10 col-lg-7 col-xl-6 py-2 border border-light px-0">
+            ${ExploreComponent()}
+         </div>
+        <div class="d-none d-lg-block col-lg-4 py-2">
+            ${WhoToFollowList()}
+        </div>
+   `);
+}
+$(exploreComponent);
